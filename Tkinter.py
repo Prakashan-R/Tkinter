a0e683e71fe3a0383b061d809cd68e698df9ba6b
Prakash R Vijay <prakashr714@gmail.com>
2020-07-10 11:31:17 +0530

    creat a main window

---
+++ Tkinter.py
@@ -0,0 +1,7 @@
+# creating a GUI window using Python Tkinter library
+from tkinter import *
+window=Tk()  #frame creat cheyyan oru class ne call cheyyanam athanu Tk()
+
+
+
+window.mainloop()  # ithinte upayogam ee program start cheitha muthal end cheyyunna vare main window closs agthirikkananu
