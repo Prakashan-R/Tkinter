7c7e56391c750f1b381ba9384efa550ba7c86d75
Prakash R Vijay <prakashr714@gmail.com>
2020-07-10 11:38:18 +0530

    aplaying size created windo

--- Tkinter.py
+++ Tkinter.py
@@ -2,6 +2,6 @@
 from tkinter import *
 window=Tk()  #frame creat cheyyan oru class ne call cheyyanam athanu Tk()
 
-
+window.geometry('800x400') # ithu upayogikkunnathu creat cheitha windowinde size (width & hight) kramikarikan
 
 window.mainloop()  # ithinte upayogam ee program start cheitha muthal end cheyyunna vare main window closs agthirikkananu
